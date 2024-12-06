318b0bf03306b7c21ac544ae1efb295fdc5cf693
BuildTools <unconfigured@null.spigotmc.org>
2024-12-05 22:56:08 -0500

    Project Setup

---
+++ src/main/java/com/zibada/zibadaCore/ZibadaCore.java
@@ -0,0 +1,17 @@
+package com.zibada.zibadaCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZibadaCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
